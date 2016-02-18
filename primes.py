6ec140191b4b17b0b8e66dbc6db147a860284b71
99YuraniPalacios <yumepa.mp@gmail.com>
2016-02-18 10:42:30 -0500

    18 de Febrero

--- primes.py
+++ primes.py
@@ -6,8 +6,18 @@ def minimo (x,y):
 
 s= minimo (2, 7)
 
-def mdc (x,y):
+def mcd (x,y):
     m=minimo(x,y)
     for i in range (m,0,-1):
         if x%i==0 and y%i==0:
            return i 
+
+
+def arecoprime (a,b):
+    m=mcd(a,b)
+    if m==1:
+       return 1
+    else:
+       return 0
+
+print arecoprime(23,7)

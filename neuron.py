356fbcdf4d0b7961d169382d48466e983aaa5e1f
99YuraniPalacios <yumepa.mp@gmail.com>
2016-04-04 13:10:19 -0500

    Taller 5 en Clase

--- neuron.py
+++ neuron.py
@@ -1,33 +1,62 @@
 # Creado el 29 de Marzo de 2016
 # Autora: Yurani Melisa Palacios
 
+
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import sys
-from scipy.interpolate omport interpld
+from scipy.interpolate import interp1d
 
-T = 6.3
-phi = 3 **((T-6.3)/10)
+delra1 = 0.003
+delta2 = 0.001
 
-V = -65
-an = phi * (0.01 * (V + 10)/(np.exp (((V+10)/10)- 1)))
+CM = 0.1 
+I = 15 
 
-Bn = phi * (0.125 * (np.exp(V/80)))
+V0 = -65 
+VK = -77 
+VNa = 50 
+VL = -54.4 
+
+gK = 36 
+gNa = 120 
+gL = 0.3 
 
 n0 = 0.317
-delta = 0.001
-n1 = (an * (1 - n0) - Bn * n0) * delta + n0
+m0 = 0.05
+h0 = 0.6
 
-I = 15
-gk = 36
-Vk = -77
-gNa = 120
-VNa = 50
-gL = 0.3
-VL = -54.4
+T = 6.3 
+phi = 3 **((T-6.3)/10) 
 
-V1 = (I - gk ** (n ** 4) * (V - Vk) - gNa ** ((m**3) * h) * (V - VNa) - gL(V - VL)
+def fan(v):
+	return phi * ((0.01 * (v + 10))/(np.exp (((v+10)/10)- 1))
+def fam(v):
+	return phi * ((0.01 * (v + 25))/(np.exp (((v+25)/10)- 1))
+def fah(v):
+	return phi * (0.07 * np.exp(v/29))
+
+
+def fbn(v):
+	return phi * (0.125 * np.exp(v/80))
+def fbm(v):
+	return phi * (4 * np.exp(v/18))
+def fbh(v):
+	return phi * (1/(np.exp((v+30)/10)+1))
+
+	
+def fn(n):
+	return fan(V0) * (1-n) - fbn(V0)*n
+	
+def fm(m):
+	return fam(V0) * (1-m) - fbm(V0)*m
+def fh(h):
+	return fah(V0) * (1-h) - fbh(V0)*h
+
+def fv(v):
+	s = (I - (gK**(fn(n0)**4))*(v-VK) - (gNa**((fm(m0)**3)*fh(h0)))*(v-VNa) - ((gL)*(v-VL))) / CM
+	
 
 
 

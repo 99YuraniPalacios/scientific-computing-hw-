72c58e64cd0e4e1fdd8ecf54a080262cf2269fac
99YuraniPalacios <yumepa.mp@gmail.com>
2016-04-07 11:14:25 -0500

    Taller 7 en Clase

---
+++ Rwalk.py
@@ -0,0 +1,9 @@
+# Fecha: 7 de Abril 2016
+# Autora: Yurani Melisa Palacios 
+
+# Create a funtion=> def rwalk1d(N):
+
+# That returns the final positions of a random walker after N iterations. After each position, the new step 
+# is chosen using a fair-coin 
+
+

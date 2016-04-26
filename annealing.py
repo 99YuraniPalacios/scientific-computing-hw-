bccf246516026a0d56fa3a64ed886083df04ad54
99YuraniPalacios <yumepa.mp@gmail.com>
2016-04-26 11:36:36 -0500

    Taller 10 en Clase

---
+++ annealing.py
@@ -0,0 +1,33 @@
+# Fecha : 26 de Abril 2016 
+# Autora: Yurani Melisa Palacios Palacios 
+
+import numpy as np
+import matplotlib 
+import matplotlib.pyplot as plt 
+
+# Problems 
+
+# 1. Created a funcion called cost that returns the cost for a given solution. In this cae,
+# the cost is given by Eq.(2)
+# cost (solution)
+
+# 2. Make a plot of the cost for solutions in the range -3 o 3, and identify a range for the
+# optimal value.
+
+
+# 3. Create a function that returns the a new solution given a possible one
+# def neighbor (solution):
+#	step_size = 1.0
+#	return (2 * np.random.random() - 1) * step_size + solution 
+
+# 4. Start with a randomly selected solution in the range found in step 4. FixT = 1 and Tmin = 10^-5
+
+
+# 5. Generate a new solution with the function neighbor
+
+# 6. Decide which of the two solutions is the best one based on what we discussed earlier (See Eq. 1)
+
+# 7. Repeat steps 5 & 6 100 times
+
+# 8. Decrease T by a factor of 0.9, and repeat steps 5-7 until the temperature drops below Tmin 
+

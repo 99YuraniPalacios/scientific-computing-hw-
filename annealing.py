3b1b0f9b06d807501a33dd272c16cb8304a7f8c0
99YuraniPalacios <yumepa.mp@gmail.com>
2016-04-28 11:34:51 -0500

    Taller 10 en Clase

--- annealing.py
+++ annealing.py
@@ -11,23 +11,51 @@ import matplotlib.pyplot as plt
 # the cost is given by Eq.(2)
 # cost (solution)
 
+def cost(solution):
+	Eq2 = np.exp(-(solution -1) **2) * np.sin(8 * solution) + 1
+
+	return Eq2 
+ 
 # 2. Make a plot of the cost for solutions in the range -3 o 3, and identify a range for the
 # optimal value.
 
+x= np.linspace(-3, 3, num= 100, endpoint= 'true')
+y= range (len(x))
+
+for i in range (len(x)):
+	y[i] = cost (x[i]) 
+
+plt.plot (x, y)
+plt.show()
 
 # 3. Create a function that returns the a new solution given a possible one
 # def neighbor (solution):
 #	step_size = 1.0
 #	return (2 * np.random.random() - 1) * step_size + solution 
 
-# 4. Start with a randomly selected solution in the range found in step 4. FixT = 1 and Tmin = 10^-5
+def neighbor (solution):
+	step_size = 1.0
+	
+	return (2 * np.random.random() - 1) * step_size + solution 
 
+# 4. Start with a randomly selected solution in the range found in step 2. FixT = 1 and Tmin = 10^-5
+
+c = np.random.uniform (-3, 3)
+T = 1
+Tmin = (10 ** -5)
 
 # 5. Generate a new solution with the function neighbor
 
+for j in range (100):
+	c2 = neighbor(c)
+
 # 6. Decide which of the two solutions is the best one based on what we discussed earlier (See Eq. 1)
+
+for k in range (100):
+	Eq1 = ((c - c2) / T)
 
 # 7. Repeat steps 5 & 6 100 times
 
 # 8. Decrease T by a factor of 0.9, and repeat steps 5-7 until the temperature drops below Tmin 
+
 

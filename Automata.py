3843deb5f07db1104707feebf7246eb4dcd81fdb
99YuraniPalacios <yumepa.mp@gmail.com>
2016-05-11 22:06:04 -0500

    Taller 11 en Clase

--- Automata.py
+++ Automata.py
@@ -15,9 +15,6 @@ n = 100
 
 w = np.zeros ([n, n], dtype = np.int8)
 
-plt.plot (w)
-plt.show()
-
 #2. We will make use of the following notation for the directions (0: east), (1: south), (2: west) and 
 # (3: north). Design a function that given a direction d and the color c of the cell in which the ant 
 # is stading, decides the next direction along which the ant will move.
@@ -47,11 +44,21 @@ def nextdirection (d, c):
 			d == Este
 	return d 
 
+
+
+def validindex(i,j, n):
+	if i < 0 or j<0 or i >= n or j >= n:
+		return 1
+	else:
+		return 0
+
 #3. Write a function that given a that given a direction of motion d and a position on the grid (i, j), 
 # returns the next position to wich the ant will move 
 # def moveforward (i, j, d): 
 
-def moveforward (i, j, d): 
+def moveforward (i, j, d):
+
+	c = w[i][j] 
 	k[i, j] = 0
 
 	if d == Este :
@@ -71,13 +78,30 @@ def moveforward (i, j, d):
 
 def validindex (i, j, n):
 
-	for i in range (20000):
-		
+	for t in range (20000):
+		c = w[i,j]
+		newd = nextdirection(d,c)
+		i1, j1 = moveforward(i,j,newd)
+		validar = validindex(i1,j1,n)
+
+		if validar == 1:
+			break
+		else:
+			if w[i,j] == 0:
+				w[i,j] = 1
+			else:
+				w[i,j] = 0
+			i = i1
+			j = j1
+			d = newd
 
 #5. Starting with i = n/2, j = n/2 and d= 0, simulate 20000 steps of the ant, or until it leaves the grid 
 
 #6. Plot the final configuration of the grid 
 # plt.imshow(w, interpolation='none', cmap= plt.cm.Greys, estent= (0, n, 0, n))
 
- plt.imshow(k, interpolation='none', cmap=plt.cm.Greys, extent=(0,n,0,n))
- plt.show()
+ #plt.imshow(k, interpolation='none', cmap=plt.cm.Greys, extent=(0,n,0,n))
+ #plt.show()
+
+plt.imshow(w, interpolation = 'none', cmap = plt.cm.Greys, extent = (0,n,0,n))
+plt.show()

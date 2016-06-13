b96f06558d7aaaaf3f29ad1ef8b7883da167915c
99YuraniPalacios <yumepa.mp@gmail.com>
2016-06-12 19:27:15 -0500

    Problema 10

---
+++ MetGauss.py
@@ -0,0 +1,8 @@
+# Jun/12/2016
+# Yurani Melisa Palacios Palacios 
+
+# Advance Problem 
+# Design al algorithm to implement the method of Gaussian quadrature. Use to create a function that calculates Li(x) 
+# Diseno al algoritmo para implementar el metodo del cuadratura de Gauss. Se utiliza para crear una funcion que calcula Li(x)
+
+

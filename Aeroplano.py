9428267eeaac74663b2fbea45a84444617baa585
99YuraniPalacios <yumepa.mp@gmail.com>
2016-06-15 10:19:26 -0500

    Aeroplano

--- Aeroplano.py
+++ Aeroplano.py
@@ -2,8 +2,8 @@
 # Yurani Melisa Palacios Palacios 
 
 import numpy 
-import matplotlib.pyplot # as plt
-# from matplotlib import pyplot
+# import matplotlib.pyplot  as plt
+from matplotlib import pyplot
 
 T = 100
 dt = 0.02
@@ -27,6 +27,16 @@ for n in range (1, N):
 	z[n] = u[0]
 
 
-matplotlib.pyplot.plot(t,z,'b' )
-matplotlib.pyplot.show()
+# matplotlib.pyplot.plot(t,z,'b' )
+# matplotlib.pyplot.show()
+
+pyplot.figure(figsize=(10,4)) # Ajusta el tamano de la grafica si quiero volverlo mas cuadrado
+pyplot.ylim(40,160) # Limite de rango en el eje y
+pyplot.tick_params(axis="both", labelsize=14)
+pyplot.xlabel("t", fontsize=14)
+pyplot.ylabel("z", fontsize=14)
+pyplot.plot(t,z, 'b-');
+pyplot.show()
+
+
 
